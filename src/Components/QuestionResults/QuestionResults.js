fb099c04af1716d24a3b4864f8e7c0bb1984f6a5
Satish Ganesan <ganesansatishm7@gmail.com>
2022-12-22 14:22:50 +0530

    debug the ask question without login

--- src/Components/QuestionResults/QuestionResults.js
+++ src/Components/QuestionResults/QuestionResults.js
@@ -1,29 +1,34 @@
-import React, { useEffect, useState } from 'react'
-import { useNavigate, useParams } from 'react-router';
-import {SingleQuestion} from '../QuestionsList/QuestionsList';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router";
+import { SingleQuestion } from "../QuestionsList/QuestionsList";
+import axios from "axios";
 import ArrowCircleLeftIcon from "@mui/icons-material/ArrowCircleLeft";
+import { useSelector } from "react-redux";
+import { login } from "../../features/loginSlice";
 
 function QuestionResults() {
-    const [results,setResults]= useState([]);
-    const navigateTo = useNavigate();
-    const {tag} = useParams();
-    async function getResults(){
-        try {
-            const response = await axios.get("https://cloneoverflow.onrender.com/questions/get-results?tag="+tag);
-            
-             setResults(response.data.results);    
-        } catch (error) {
-            console.log(error);
-        }
+  const loginStatus = useSelector((state) => state.login.value);
+  const [results, setResults] = useState([]);
+  const navigateTo = useNavigate();
+  const { tag } = useParams();
+  async function getResults() {
+    try {
+      const response = await axios.get(
+        "https://cloneoverflow.onrender.com/questions/get-results?tag=" + tag
+      );
+
+      setResults(response.data.results);
+    } catch (error) {
+      console.log(error);
     }
-    useEffect(()=>{
-          getResults();
-    },[tag]);
-    
+  }
+  useEffect(() => {
+    getResults();
+  }, [tag]);
+
   return (
     <div className="wrapper container">
-         <ArrowCircleLeftIcon
+      <ArrowCircleLeftIcon
         style={{
           marginLeft: "10%",
           fontSize: "40px",
@@ -32,40 +37,41 @@ function QuestionResults() {
           navigateTo("/");
         }}
       />
+      <div className="row">
+        <div className="col-12 mt-5">
+          <div className="header">
+            <h1>Results</h1>
+            <button
+              type="button"
+              className="btn btn-primary rounded-0"
+              onClick={() => {
+                loginStatus
+                  ? navigateTo("/ask-question")
+                  : navigateTo("/login");
+              }}
+            >
+              ASK
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="questions-list">
+        <div className="container">
+          <h4>{results.length} &nbsp; questions</h4>
           <div className="row">
-            <div className="col-12 mt-5">
-              <div className="header">
-                <h1>Results</h1>
-                <button
-                  type="button"
-                  className="btn btn-primary rounded-0"
-                  onClick={() => {
-                     navigateTo("/ask-question"); 
-                  }}
-                >
-                  ASK
-                </button>
-              </div>
+            <div className="col-md-2 col-xs-0"></div>
+            <div className="col-md-10 col-12">
+              {!results ? (
+                <h1>Loading...</h1>
+              ) : (
+                results.map((d, i) => <SingleQuestion key={i} d={d} />)
+              )}
             </div>
           </div>
-          <div className="questions-list">
-          <div className="container">
-      <h4>{results.length} &nbsp; questions</h4>
-      <div className="row">
-      <div className="col-md-2 col-xs-0">
-      </div>
-      <div className="col-md-10 col-12">
-        {!results ? (
-        <h1>Loading...</h1>
-      ) : (
-        results.map((d, i) => <SingleQuestion key={i} d={d} />)
-      )}
         </div>
       </div>
     </div>
-          </div>
-        </div>
-  )
+  );
 }
 
-export default QuestionResults
+export default QuestionResults;
